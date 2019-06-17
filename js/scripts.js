dc52758a3e51e69e2bc8ff4889357c19d7e3060c
Lucas Blasdel and Saswati Patra <lucas@>
2019-06-17 11:52:31 -0700

    created a location bok contains all the places we visited

--- js/scripts.js
+++ js/scripts.js
@@ -1,124 +1,127 @@
-// Business Logic for AddressBook ---------
-function AddressBook() {
-  this.contacts = [],
+// Business Logic for LocationBook ---------
+function LocationBook() {
+  this.places = [],
   this.currentId = 0
 }
 
-AddressBook.prototype.addContact = function(contact) {
-  contact.id = this.assignId();
-  this.contacts.push(contact);
+LocationBook.prototype.addPlace = function(place) {
+  place.id = this.assignId();
+  this.places.push(place);
 }
-// this will attempt to update the contact in the address Book.
-AddressBook.prototype.updateContact = function(id) {
-  var updateFirst= prompt("please enter a new first name:")
-  var updateLast= prompt("please enter a new Last name:")
-  var updateNumber= prompt("please enter a new number:")
-  for (var i=0; i< this.contacts.length; i++) {
-    if (this.contacts[i]) {
-      if (this.contacts[i].id == id) {
-
-        this.contacts[i].firstName = updateFirst;
-        this.contacts[i].lastName = updateLast;
-        this.contacts[i].phoneNumber = updateNumber;
-      }
-    }
-  };
-}
-
-AddressBook.prototype.assignId = function() {
+// // this will attempt to update the contact in the address Book.
+// LocationBook.prototype.updateContact = function(id) {
+//   var updateFirst= prompt("please enter a new first name:")
+//   var updateLast= prompt("please enter a new Last name:")
+//   var updateNumber= prompt("please enter a new number:")
+//   for (var i=0; i< this.contacts.length; i++) {
+//     if (this.contacts[i]) {
+//       if (this.contacts[i].id == id) {
+//
+//         this.contacts[i].firstName = updateFirst;
+//         this.contacts[i].lastName = updateLast;
+//         this.contacts[i].phoneNumber = updateNumber;
+//       }
+//     }
+//   };
+// }
+//
+LocationBook.prototype.assignId = function() {
   this.currentId += 1;
   return this.currentId;
 }
 
-AddressBook.prototype.findContact = function(id) {
-  for (var i=0; i< this.contacts.length; i++) {
-    if (this.contacts[i]) {
-      if (this.contacts[i].id == id) {
-        return this.contacts[i];
+LocationBook.prototype.findPlace = function(id) {
+  for (var i=0; i< this.places.length; i++) {
+    if (this.places[i]) {
+      if (this.places[i].id == id) {
+        return this.places[i];
       }
     }
   };
   return false;
 }
 
-AddressBook.prototype.deleteContact = function(id) {
-  for (var i=0; i< this.contacts.length; i++) {
-    if (this.contacts[i]) {
-      if (this.contacts[i].id == id) {
-        delete this.contacts[i];
+LocationBook.prototype.deletePlace = function(id) {
+  for (var i=0; i< this.places.length; i++) {
+    if (this.places[i]) {
+      if (this.places[i].id == id) {
+        delete this.places[i];
         return true;
       }
     }
   };
   return false;
 }
-function attachContactListeners() {
-  $("ul#contacts").on("click", "li", function() {
-    showContact(this.id);
+function attachPlaceListeners() {
+  $("ul#places").on("click", "li", function() {
+    showPlace(this.id);
   });
   $("#buttons").on("click", ".deleteButton", function() {
-    addressBook.deleteContact(this.id);
-    $("#show-contact").hide();
-    displayContactDetails(addressBook);
+    locationBook.deletePlace(this.id);
+    $("#show-place").hide();
+    displayPlaceDetails(locationBook);
   });
 
-  $("#buttons").on("click", ".updateButton", function() {
-    addressBook.updateContact(this.id);
-    $("#show-contact").hide();
-    displayContactDetails(addressBook);
-  });
 };
 
 
-// Business Logic for Contacts ---------
-function Contact(firstName, lastName, phoneNumber) {
-  this.firstName = firstName,
-  this.lastName = lastName,
-  this.phoneNumber = phoneNumber
+// Business Logic for Place ---------
+function Place(country, city, landmark, date, notes) {
+  this.country = country,
+  this.cityName = city,
+  this.landmarks = landmark,
+  this.date = date,
+  this.notes = notes
 }
 
-Contact.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
+Place.prototype.fullLocation = function() {
+  return this.cityName + ", " + this.country;
 }
-function showContact(contactId) {
-  var contact = addressBook.findContact(contactId);
-  $("#show-contact").show();
-  $(".first-name").html(contact.firstName);
-  $(".last-name").html(contact.lastName);
-  $(".phone-number").html(contact.phoneNumber);
+function showPlace(placeId) {
+  var place = locationBook.findPlace(placeId);
+  $("#show-place").show();
+  $(".country").html(place.country);
+  $(".cityName").html(place.cityName);
+  $(".landmarks").html(place.landmarks);
+  $(".date").html(place.date);
+  $(".notes").html(place.notes);
   var buttons = $("#buttons");
   buttons.empty();
-  buttons.append("<button class='deleteButton' id=" +  + contact.id + ">Delete</button>");
-  buttons.append("<button class='updateButton' id=" +  + contact.id + ">update</button>");
+  buttons.append("<button class='deleteButton' id=" +  + place.id + ">Delete</button>");
+
 }
 
 // User Interface Logic ---------
-var addressBook = new AddressBook();
+var locationBook = new LocationBook();
 
-function displayContactDetails(addressBookToDisplay) {
-  var contactsList = $("ul#contacts");
-  var htmlForContactInfo = "";
-  addressBookToDisplay.contacts.forEach(function(contact) {
-    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+function displayPlaceDetails(locationBookToDisplay) {
+  var placesList = $("ul#places");
+  var htmlForPlaceInfo = "";
+  locationBookToDisplay.places.forEach(function(place) {
+    htmlForPlaceInfo += "<li id=" + place.id + ">" + place.cityName + ", " + place.country + "</li>";
   });
-  contactsList.html(htmlForContactInfo);
+  placesList.html(htmlForPlaceInfo);
 };
 
 $(document).ready(function() {
-  attachContactListeners();
-  $("form#new-contact").submit(function(event) {
+  attachPlaceListeners();
+  $("form#new-location").submit(function(event) {
     event.preventDefault();
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
-    var inputtedPhoneNumber = $("input#new-phone-number").val();
+    debugger;
+    var inputtedCountry = $("input#new-country").val();
+    var inputtedCityName = $("input#new-cityName").val();
+    var inputtedLandmark = $("input#new-landmarks").val();
+    var inputtedDate = $("input#new-date").val();
+    var inputtedNote = $("input#new-notes").val();
 
-    // The next three lines are new:
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input#new-phone-number").val("");
+    $("input#new-country").val("");
+    $("input#new-cityName").val("");
+    $("input#new-landmarks").val("");
+    $("input#new-date").val("");
+    $("input#new-notes").val("");
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
-    addressBook.addContact(newContact);
-    displayContactDetails(addressBook);
+    var newPlace = new Place(inputtedCountry, inputtedCityName, inputtedLandmark,inputtedDate, inputtedNote);
+    locationBook.addPlace(newPlace);
+    displayPlaceDetails(locationBook);
   })
 })
